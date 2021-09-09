45d7e7491ed2780fe99ed0216fcca34017bdde9e
ryanalleyne <34393719+ryanalleyne@users.noreply.github.com>
2021-09-09 17:26:04 +0100

    Add files via upload
    startup script

---
+++ brew.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Install Brew Packages
+brew install python
+brew install tree
+
+# Install MacOS Applications
+brew install google-chrome
+brew install firefox
+brew install opera
+brew install chrome-devtools
+brew install tor-browser
+
+brew install vlc
+brew install flux
+
+brew install intellij-idea-ce
+brew install visual-studio-code
+brew install iterm2
+brew install atom
+brew install sublime-text
+brew install notion
+
+brew install slack
+brew install spotify
+brew install kindle
+brew install calibre
+
+brew install zoom
+brew install telegram
+brew install whatsapp
+brew install discord
+brew install lastpass
+
+brew install microsoft-teams
+brew install microsoft-word
+brew install microsoft-excel
+brew install microsoft-powerpoint
+brew install adobe-creative-cloud
+
+brew install steam
+
+# Install Source Code Pro Font
+brew tap homebrew/cask-fonts
+brew install font-source-code-pro
